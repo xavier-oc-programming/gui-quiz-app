da86e7ab7ea5e59ad14a1373ce673d2f2ca79cb8
Xavier OC <174115638+xavier-oc-programming@users.noreply.github.com>
2026-04-06 16:57:21 +0200

    Remove course files from root: preserved in original/ and original branch

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from data import question_data
-from question_model import Question
-from quiz_brain import QuizBrain
-from ui import QuizInterface
-
-question_bank = [Question(q["question"], q["correct_answer"]) for q in question_data]
-quiz = QuizBrain(question_bank)
-QuizInterface(quiz)
--- question_model.py
+++
@@ -1,5 +0,0 @@
-# question_model.py
-class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
